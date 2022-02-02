7a1f09c2f7566074586bf13d1ae57a1e4281a400
vijaythumar <Vijaythumar1020@gmail.com>
2022-02-02 10:18:17 +0530

    edited code

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola from Vijay.
-        </a>
+          Hola From Vijay
       </header>
     </div>
   );
