42f588d0d96bc1aef19f8ecc5b8934d7476044dc
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-02-10 20:24:17 +0530

    prc3 final code

---
+++ React-js-Practical3/src/components/UserTodo.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './UserTodo.css'
+
+
+function UserTodo(props){
+
+    const [checked, setChecked] = useState(false);
+
+    function changeStatus() {
+        setChecked(!checked);
+    }
+
+    return(
+        <React.Fragment>
+            <div className='usertodo'>
+                <span className={checked ? 'strike' : ''}>{props.task}</span>
+                {/* <span className='date'>{props.date.toString()}</span> */}
+                <input className='chkbox' type={'checkbox'} onClick={changeStatus}/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default UserTodo;
+
+
---
+++ React-js-prc3/src/App.js
@@ -0,0 +1,14 @@
+import MainContainer from "./components/MainContainer";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <MainContainer /> 
+        {/* < button /> */}
+    </div>
+  );
+}
+
+export default App;
